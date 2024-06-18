1a428d8eb33dbf91ce55fefde288d17815f3a521
myokss <myokkeu@gmail.com>
2024-06-18 18:41:03 +0800

    first commit

---
+++ app/Http/Controllers/GenresController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Genres;
+use App\Http\Requests\StoreGenresRequest;
+use App\Http\Requests\UpdateGenresRequest;
+
+class GenresController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreGenresRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Genres $genres)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Genres $genres)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateGenresRequest $request, Genres $genres)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Genres $genres)
+    {
+        //
+    }
+}
